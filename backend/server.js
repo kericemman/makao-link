e2149f7f2956d29ddea6b4683e4242b1c25178be
VERSER <user@users-MacBook-Pro.local>
2026-04-27 11:24:53 +0200

    Backend access

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+app.listen(PORT, "127.0.0.1", () => {
   console.log(`Server running on port ${PORT}`);
 });
